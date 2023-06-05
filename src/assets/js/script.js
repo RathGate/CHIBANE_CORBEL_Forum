faf2fc1246f840f92f95c4583426cb7242600592
evzs <szve@proton.me>
2023-06-05 17:29:10 +0200

    feurum error

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -47,13 +47,12 @@ closeBtnLogin.addEventListener('click', function() {
 
     return "";
 }*/
-
 function validatePassword(password) {
     if (password.length < 8) {
         return "Password must be at least 8 characters long";
     }
 
-    const passwordRegex = /"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"/;
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     if (!passwordRegex.test(password)) {
         return "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character";
     }
@@ -72,29 +71,30 @@ function validateEmail(email) {
 
 function passwordValidation() {
     const password = document.getElementById("password").value;
-
     const passwordError = validatePassword(password);
+    const passwordErrorElem = document.getElementById("passwordError");
 
     if (passwordError) {
-        document.getElementById("passwordError").textContent = passwordError;
+        passwordErrorElem.textContent = passwordError;
+        passwordErrorElem.style.display = "flex";
         return false;
     }
 
+    passwordErrorElem.style.display = "none";
     return true;
 }
 
 function emailValidation() {
     const email = document.getElementById("email").value;
-    /*    const username = document.getElementById("username").value;*/
-
     const emailError = validateEmail(email);
-    /*    const usernameError = validateUsername(username);*/
+    const emailErrorElem = document.getElementById("emailError");
 
-    if (emailError /*|| usernameError*/) {
-        document.getElementById("emailError").textContent = emailError;
-        /*        document.getElementById("usernameError").textContent = usernameError;*/
+    if (emailError) {
+        emailErrorElem.textContent = emailError;
+        emailErrorElem.style.display = "flex";
         return false;
     }
 
+    emailErrorElem.style.display = "none";
     return true;
 }
